44638bceaae1e560725c89e10d18a61aa74f65a8
Sebastian Hilger <hilger@kraenk.de>
2022-07-11 17:44:40 +0200

    remove reordering

--- src/Observers/WorkObserver.php
+++ src/Observers/WorkObserver.php
@@ -24,7 +24,7 @@ class WorkObserver
 
     public function saved(Work $work)
     {
-        Slideshow::find($work->slideshow_id)->refreshWorksOrder();
+        // Slideshow::find($work->slideshow_id)->refreshWorksOrder();
     }
 
     protected function ensureOnlyOneArtistPortfolioImage($work)

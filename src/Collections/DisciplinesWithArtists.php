698542bd06b8892e2c2738033e493fa0a76f1e6c
Sebastian Hilger <hilger@kraenk.de>
2022-08-24 22:52:08 +0200

    sort by last name nav

--- src/Collections/DisciplinesWithArtists.php
+++ src/Collections/DisciplinesWithArtists.php
@@ -3,6 +3,7 @@
 namespace Kraenkvisuell\NovaCmsPortfolio\Collections;
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 use Kraenkvisuell\NovaCmsPortfolio\Models\Discipline;
 
 class DisciplinesWithArtists
@@ -10,7 +11,7 @@ class DisciplinesWithArtists
     public static function get()
     {
         //return Cache::tags('artists')->rememberForever('DisciplinesWithArtists.'.app()->getLocale(), function () {
-            $disciplines = Discipline::ordered()
+        $disciplines = Discipline::ordered()
                 ->has('artists')
                 ->with(['artists' => function ($b) {
                     $b->where('is_published', true)
@@ -24,50 +25,55 @@ class DisciplinesWithArtists
                 }])
                 ->get();
 
-            $results = [];
+        $results = [];
 
-            foreach ($disciplines as $discipline) {
-                $artists = [];
+        foreach ($disciplines as $discipline) {
+            $artists = [];
 
-                foreach ($discipline->artists as $artist) {
-                    $portfolioImages = [];
+            foreach ($discipline->artists as $artist) {
+                $portfolioImages = [];
 
-                    foreach ($artist->portfolioImages() as $portfolioImage) {
-                        $item = [
-                            'imgUrls' => [],
-                            'ratio' => $portfolioImage ? nova_cms_ratio($portfolioImage) : 1,
-                        ];
+                foreach ($artist->portfolioImages() as $portfolioImage) {
+                    $item = [
+                        'imgUrls' => [],
+                        'ratio' => $portfolioImage ? nova_cms_ratio($portfolioImage) : 1,
+                    ];
 
-                        foreach (config('nova-cms-media.resize.sizes') ?: [] as $sizeKey => $sizeValue) {
-                            if ($portfolioImage) {
-                                $item['imgUrls'][$sizeKey] = nova_cms_image($portfolioImage, $sizeKey);
-                            } else {
-                                $item['imgUrls'][$sizeKey] = nova_cms_empty_image();
-                            }
+                    foreach (config('nova-cms-media.resize.sizes') ?: [] as $sizeKey => $sizeValue) {
+                        if ($portfolioImage) {
+                            $item['imgUrls'][$sizeKey] = nova_cms_image($portfolioImage, $sizeKey);
+                        } else {
+                            $item['imgUrls'][$sizeKey] = nova_cms_empty_image();
                         }
-
-                        $portfolioImages[] = $item;
                     }
 
-                    $artists[] = [
-                        'id' => $artist->id,
-                        'slug' => $artist->slug,
-                        'name' => $artist->name,
-                        'portfolioImages' => $portfolioImages,
-                    ];
+                    $portfolioImages[] = $item;
                 }
 
-                $result = [
-                    'id' => $discipline->id,
-                    'title' => $discipline->title,
-                    'slug' => $discipline->slug,
-                    'artists' => collect($artists)->sortBy('name')->values()->all(),
+                $artists[] = [
+                    'id' => $artist->id,
+                    'slug' => $artist->slug,
+                    'name' => $artist->name,
+                    'portfolioImages' => $portfolioImages,
                 ];
-
-                $results[] = $result;
             }
 
-            return $results;
+            $result = [
+                'id' => $discipline->id,
+                'title' => $discipline->title,
+                'slug' => $discipline->slug,
+                'artists' => collect($artists)
+                    ->sortBy(function ($result) {
+                        return Str::of($result['name'])->afterLast(' ');
+                    })
+                    ->values()
+                    ->all(),
+            ];
+
+            $results[] = $result;
+        }
+
+        return $results;
         //});
     }
 }
